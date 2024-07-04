0ce383adefe37a9613bad6787cd534508a0c92b9
abhishekkumarchaudhary1 <abhishek.chaudhary33@gmail.com>
2024-07-04 12:42:48 +0530

    optimizing the code and doing minor bug fixes

--- src/components/cityTrivia/CityTrivia.jsx
+++ src/components/cityTrivia/CityTrivia.jsx
@@ -3,6 +3,7 @@ import { FaQuoteLeft } from 'react-icons/fa';
 
 const CityTrivia = ({ cityName }) => {
   const [trivia, setTrivia] = useState('');
+  const [error, setError] = useState(null);
 
   const getCityTrivia = async (cityName) => {
     try {
@@ -10,25 +11,40 @@ const CityTrivia = ({ cityName }) => {
         `https://en.wikipedia.org/api/rest_v1/page/summary/${cityName}`
       );
       const data = await response.json();
-      setTrivia(data.extract);
+      if (response.ok) {
+        setTrivia(data.extract);
+        setError(null);
+      } else {
+        throw new Error(data.detail);
+      }
     } catch (error) {
-      console.error('Error fetching city trivia:: ', error);
+      setError('No trivia found. Check your search');
+      setTrivia('');
     }
   };
 
   useEffect(() => {
-    getCityTrivia(cityName);
+    if (cityName) {
+      getCityTrivia(cityName);
+    }
   }, [cityName]);
 
   return (
-    <section className='triviaSection w-[80%] max-w-96 my-3 mx-auto'>
-        <div className='card bg-white text-2xl flex-col justify-between items-center my-3 p-2 rounded-md drop-shadow-md'>
+    <>
+      {trivia ? (
+        <section className='triviaSection w-[80%] max-w-96 my-3 mx-auto'>
+          <div className='card bg-white text-2xl flex-col justify-between items-center my-3 p-2 rounded-md drop-shadow-md'>
             <FaQuoteLeft />
             <h2>Trivia</h2>
             <p className='text-sm bg-slate-500 text-white p-3 rounded md:text-md lg:text-lg'>{trivia}</p>
-        </div>
-
-    </section>
+          </div>
+        </section>
+      ) : (
+        <h2 className='bg-slate-500 text-white w-[80%] max-w-96 my-4 mx-auto p-3 text-center text-sm rounded'>
+          {error || 'Loading...'}
+        </h2>
+      )}
+    </>
   );
 };
 
--- src/components/cityWeather/CityWeather.jsx
+++ src/components/cityWeather/CityWeather.jsx
@@ -6,7 +6,16 @@ import { FaLocationDot } from 'react-icons/fa6';
 import { WiHumidity } from 'react-icons/wi';
 
 const CityWeather = ({ cityName }) => {
-  const [weather, setWeather] = useState({ name: '', temperature: '', condition: '', humidity: '', windSpeed: '', pressure: '' });
+  const [weather, setWeather] = useState({
+    name: '',
+    temperature: '',
+    condition: '',
+    humidity: '',
+    windSpeed: '',
+    pressure: '',
+    iconURL: ''
+  });
+  const [error, setError] = useState(null);
 
   const apiKey = import.meta.env.VITE_OPEN_WEATHER_API_KEY;
 
@@ -16,19 +25,25 @@ const CityWeather = ({ cityName }) => {
         `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`
       );
       const data = await response.json();
-    //   console.log(data);
-      setWeather({
-        name:`${(data.name.toUpperCase())}`,
-        temperature: `${Math.round(data.main.temp)}°C`,
-        condition: `${data.weather[0].main}`,
-        humidity: `${data.main.humidity}`,
-        windSpeed: `${Math.round(data.wind.speed * 3.6)} km/hr`,
-        pressure: `${Math.round(data.main.pressure)}mb`,
-
-        iconURL : `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-      });
+      if (response.ok) {
+        setWeather({
+          name: data.name.toUpperCase(),
+          temperature: `${Math.round(data.main.temp)}°C`,
+          condition: data.weather[0].main,
+          humidity: data.main.humidity,
+          windSpeed: `${Math.round(data.wind.speed * 3.6)} km/hr`,
+          pressure: `${Math.round(data.main.pressure)} mb`,
+          iconURL: `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+        });
+        setError(null);
+      } else {
+        setWeather({
+          name: ''
+        })
+        throw new Error(data.message);
+      }
     } catch (error) {
-      console.log('Error: City not found!!!');
+      setError('City not found!!!');
     }
   };
 
@@ -37,46 +52,47 @@ const CityWeather = ({ cityName }) => {
   }, [cityName]);
 
   return (
-    <section className='weatherSection w-[80%] max-w-96 my-3 mx-auto'>
-        <div className='card bg-white text-2xl flex flex-wrap justify-between items-center my-3 p-2 rounded-md drop-shadow-md'>
+    <>
+      {weather.name ? (
+        <section className='weatherSection w-[80%] max-w-96 my-3 mx-auto'>
+          <div className='card bg-white text-2xl flex flex-wrap justify-around items-center my-3 p-2 rounded-md drop-shadow-md'>
             <div className="locationDiv bg-slate-500 w-[100px] h-[100px] flex-col justify-between items-center my-3 mx-auto p-3 rounded-sm">
-                <FaLocationDot className='text-white' />
-                <h1 className=' text-white font-bold rounded-sm'>{weather.name}</h1>
+              <FaLocationDot className='text-white' />
+              <h1 className='text-white text-center font-bold text-xs rounded-sm'>{weather.name}</h1>
             </div>
             <div className='tempDiv w-[100px] h-[100px] flex-col justify-between items-center my-3 mx-auto p-3 rounded-sm'>
-                <CiTempHigh />
-                <p>{weather.temperature}</p>
-
+              <CiTempHigh />
+              <p>{weather.temperature}</p>
             </div>
             <div className='conditionDiv border rounded w-[100px] flex-col justify-between items-center my-3 mx-auto p-3'>
-                <img className='bg-slate-500 rounded-sm' src={weather.iconURL} alt="" />
-                <p className='text-center'>{weather.condition}</p>
-
+              <img className='bg-slate-500 rounded-sm' src={weather.iconURL} alt={weather.condition} />
+              <p className='text-center'>{weather.condition}</p>
             </div>
-
-        </div>
-        <div className='card bg-white text-2xl flex flex-wrap justify-between items-center my-3 p-2 rounded-md drop-shadow-md'>
+          </div>
+          <div className='card bg-white text-2xl flex flex-wrap justify-between items-center my-3 p-2 rounded-md drop-shadow-md'>
             <div className="humidityDiv bg-slate-500 text-white w-[100px] h-[100px] flex-col justify-between items-center my-3 mx-auto p-3 rounded-sm">
-
-                <WiHumidity className='text-sm text-center md:text-md lg:text-lg' />
-                <h2 className='text-sm text-center md:text-md'>Humidity</h2>
-                <p className='text-sm text-center md:text-md lg:text-lg'>{weather.humidity}</p>
+              <WiHumidity className='text-sm text-center md:text-md lg:text-lg' />
+              <h2 className='text-sm text-center md:text-md'>Humidity</h2>
+              <p className='text-sm text-center md:text-md lg:text-lg'>{weather.humidity}</p>
             </div>
             <div className="windSpeedDiv bg-slate-500 text-white w-[100px] h-[100px] flex-col justify-between items-center my-3 mx-auto p-3 rounded-sm">
-
-                <FaWind className='text-sm text-center md:text-md lg:text-lg' />
-                <h2 className='text-sm text-center md:text-md'>Wind Speed</h2>
-                <p className='text-sm text-center md:text-md lg:text-lg'>{weather.windSpeed}</p>
+              <FaWind className='text-sm text-center md:text-md lg:text-lg' />
+              <h2 className='text-sm text-center md:text-md'>Wind Speed</h2>
+              <p className='text-sm text-center md:text-md lg:text-lg'>{weather.windSpeed}</p>
             </div>
             <div className="pressureDiv bg-slate-500 text-white w-[100px] h-[100px] flex-col justify-between items-center my-3 mx-auto p-3 rounded-sm">
-
-                <CgCompressV className='text-sm text-center md:text-md lg:text-lg' />
-                <h2 className='text-sm text-center md:text-md'>Pressure</h2>
-                <p className='text-sm text-center md:text-md lg:text-lg'>{weather.pressure}</p>
+              <CgCompressV className='text-sm text-center md:text-md lg:text-lg' />
+              <h2 className='text-sm text-center md:text-md'>Pressure</h2>
+              <p className='text-sm text-center md:text-md lg:text-lg'>{weather.pressure}</p>
             </div>
-            
-        </div>
-    </section>
+          </div>
+        </section>
+      ) : (
+        <h2 className='bg-slate-500 text-white w-[80%] max-w-96 my-4 mx-auto p-3 text-center text-sm rounded'>
+          {error ? error : 'No City found. Check your search'}
+        </h2>
+      )}
+    </>
   );
 };
 
